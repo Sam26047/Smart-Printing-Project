019cda15a955f7de98ef1423d8af149e891f70a6
Sam26047 <goelsamarth2604@gmail.com>
2026-01-11 17:10:30 +0530

    Add connection pooling and POST /print-jobs API

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,47 @@
 import express from "express";
 import pg from "pg";
+
 const app = express();
+app.use(express.json());
 
-async function testDB(retries = 5) {   //new client creation for retrying connection
-  const client = new pg.Client({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+const {Pool} = pg;
 
-  try {
-    await client.connect();
-    const res = await client.query("SELECT 1");
-    console.log("Database connected:", res.rows);
-    await client.end();
-  } catch (err) {
-    console.error("Database connection failed. Retrying...", err.code);  //keep retrying until conn successful as postgres might still be booting
-    if (retries > 0) {
-      setTimeout(() => testDB(retries - 1), 2000);
-    } else {
-      console.error("Could not connect to database after retries");
-    }
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+})
+
+app.post("/print-jobs",async(req,res)=>{
+  const {file_name,file_path,copies,color,double_sided} = req.body;
+
+  //Basic Validation
+  if(!file_name || !file_path || !copies){
+    return res.status(400).json({error:"Missing required fields"});
   }
-}
 
-testDB();
+  try{
+    const result = await pool.query(
+      `
+        INSERT INTO print_jobs
+          (file_name,file_path,copies,color,double_sided,status)
+        VALUES
+          ($1,$2,$3,$4,$5,'PENDING')
+          RETURNING id
+      `,
+      [file_name,file_path,copies,color,double_sided]
+    );
+    res.status(201).json({
+      job_id:result.rows[0].id,  //sets resource succesfully created(201) as status and returns job id to client
+    });
+  }catch(err){
+    console.error(err);
+    res.status(500).json({error:"Failed to create print job"});
+  }
+});
+
 
 app.get("/", (req, res) => {
   res.send("Backend is alive 🚀");
