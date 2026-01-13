9c6c7e0ae967782ad52251d1046bdef8d5b794b6
Sam26047 <goelsamarth2604@gmail.com>
2026-01-14 01:26:26 +0530

    Configure Multer

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,33 @@
 import express from "express";
 import pg from "pg";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = "./uploads";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: uploadDir,
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== "application/pdf") {
+      return cb(new Error("Only PDFs allowed"));
+    }
+    cb(null, true);
+  },
+});
+
 
 //6-digit OTP for collection verification
 function generateOTP() {
